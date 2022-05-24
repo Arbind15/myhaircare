ecfb55db3b606cad227c5e9762707d48b2652c74
Arbind Mehta <mehtaarbind67@gmail.com>
2022-05-24 21:35:26 +0545

    login_fixed

--- MyHair/urls.py
+++ MyHair/urls.py
@@ -23,6 +23,8 @@ from django.views.static import serve
 urlpatterns = [
                   path('', include('main_site.urls')),
                   path('admin/', admin.site.urls),
+
+                # for solving static files issues with debug=fales
                   re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
                   re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 
